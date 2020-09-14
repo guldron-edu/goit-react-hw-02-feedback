886eb67428bee3fef5a651dc8722641bc1bb4137
guldron-edu <58664019+guldron-edu@users.noreply.github.com>
2020-09-14 22:04:30 +0300

    Updates

---
+++ precache-manifest.df00a9a38f739a425a08c2e01c5623de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a1eb687b10138144129c6bac58e4b2",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "29f34f67198516ba44a7",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5bc24a30.chunk.css"
+  },
+  {
+    "revision": "8e61d9c921c530146160",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a4a121a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a4a121a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f34f67198516ba44a7",
+    "url": "/goit-react-hw-02-feedback/static/js/main.45adffdc.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
